018c9d09383e617b20e96e3f20b44c03fae9f303
David Thievon <pdkfan@gmail.com>
2025-02-15 21:47:22 +0100

    feat(cli)!: make all actions subcommands
    This cleans up the cli quite a bit, but does mean that if you want to
    view a file you now need to use the `view` subcommand, hence the
    breaking change.

--- crates/igneous-md/src/cli.rs
+++ crates/igneous-md/src/cli.rs
@@ -1,21 +1,10 @@
 //! Module containing all CLI related functionality
-use clap::{Args, Parser, Subcommand};
+use clap::{Parser, Subcommand};
 use rocket::log::LogLevel as RocketLogLevel;
-use std::{fs, path::PathBuf, result::Result, str::FromStr};
+use std::{path::PathBuf, result::Result, str::FromStr};
 
-use crate::{convert, export};
-
-#[cfg(feature = "generate_config")]
-use crate::config;
-#[cfg(feature = "generate_config")]
-use crate::paths::default_css_dir;
 #[cfg(feature = "generate_config")]
 use std::fmt::Display;
-
-// HACK: I do not like the fact that args and command are not really mutually exclusive. I couldn't
-// find a way to do this without using subcommands, which I do not want to do, since it doesn't
-// make sense to force people to use a subcommand just to view the markdown, when that is the main
-// purpose of the application.
 
 /// Top Level Struct of the CLI
 /// For more information see [clap documentation](https://docs.rs/clap/latest/clap/index.html)
@@ -23,98 +12,49 @@ use std::fmt::Display;
 #[command(version,
     about= "igneous-md | the simple and lightweight markdown viewer",
     long_about = None,
-    subcommand_negates_reqs = true,
-    // If we the fix the HACK then we could get rid of this 
-    override_usage= "igneous-md [OPTIONS] <PATH|COMMAND>"
 )]
 pub struct Cli {
     #[command(subcommand)]
-    /// Actions other than launching the server and viewer
-    pub command: Option<Action>,
-    #[command(flatten)]
-    pub args: NormalArgs,
-}
-
-/// Args used when no [Cli::command] is passed
-#[derive(Debug, Args)]
-#[group(required = true, multiple = true)]
-pub struct NormalArgs {
-    /// Path to markdown file
-    #[arg(value_name = "PATH", required = true)]
-    pub path: Option<PathBuf>,
-    /// Path to stylesheet within css dir
-    #[arg(short, long, value_name = "PATH")]
-    pub css: Option<PathBuf>,
-    /// Path to alternate css dir
-    #[arg(long, value_name = "PATH")]
-    pub css_dir: Option<PathBuf>,
-    /// Start server without viewer
-    #[arg(long, default_value = "false")]
-    #[cfg(feature = "viewer")]
-    pub no_viewer: bool,
+    /// What to do
+    pub command: Action,
     /// Log Level, aka. how verbose the application will be.
     #[arg(short, long, default_value = if cfg!(debug_assertions) {"INFO"} else {"WARN"})]
     pub log_level: UnifiedLevel,
-    /// Port to run the server on
-    #[arg(short, long, default_value = "2323")]
-    pub port: u16,
-    /// Open browser tab
-    #[arg(short, long, default_value = "false")]
-    pub browser: bool,
-}
-
-impl Cli {
-    /// Handle the different actions that need to happen if a [Cli::command] is passed
-    pub fn handle_actions(&self) -> Result<ActionResult, Box<dyn std::error::Error>> {
-        // Convert the md file rather than launching the server, if the user passed the subcommand
-        if let Some(action) = &self.command {
-            match action {
-                Action::Convert { path, export_path } => {
-                    let html = convert::md_to_html(
-                        &fs::read_to_string(path).expect("Failed to read md file to string."),
-                    );
-
-                    if let Err(e) = export::export(
-                        convert::initial_html(
-                            &self.args.css.clone().unwrap_or_default().to_string_lossy(),
-                            &html,
-                        ),
-                        export_path.clone().map(PathBuf::from),
-                    ) {
-                        log::error!("Failed to export md.");
-
-                        return Err(Box::new(e));
-                    }
-
-                    Ok(ActionResult::Exit)
-                }
-                #[cfg(feature = "generate_config")]
-                Action::GenerateConfig { overwrite } => {
-                    if default_css_dir().exists() && !overwrite {
-                        return Err(Box::new(ActionError::ConfigDirExists));
-                    }
-
-                    fs::create_dir_all(default_css_dir().join("hljs")).unwrap();
-
-                    let rt = rocket::tokio::runtime::Runtime::new().unwrap();
-                    rt.block_on(config::generate::generate_config_files(default_css_dir()))?;
-
-                    Ok(ActionResult::Exit)
-                }
-            }
-        } else {
-            Ok(ActionResult::Continue)
-        }
-    }
 }
 
 /// Actions other than launching the server to view markdown
 #[derive(Debug, Subcommand)]
 pub enum Action {
+    /// View a markdown file
+    #[command(visible_alias = "v")]
+    View {
+        /// Path to markdown file
+        #[arg(value_name = "PATH", required = true)]
+        path: PathBuf,
+        /// Path to stylesheet within css dir
+        #[arg(short, long, value_name = "PATH")]
+        css: Option<PathBuf>,
+        /// Path to alternate css dir
+        #[arg(long, value_name = "PATH")]
+        css_dir: Option<PathBuf>,
+        /// Start server without viewer
+        #[arg(long, default_value = "false")]
+        #[cfg(feature = "viewer")]
+        no_viewer: bool,
+        /// Port to run the server on
+        #[arg(short, long, default_value = "2323")]
+        port: u16,
+        /// Open browser tab
+        #[arg(short, long, default_value = "false")]
+        browser: bool,
+    },
     /// Convert a md file to html and save it to disk
     Convert {
-        /// The file to export
+        /// The file to convert
         path: PathBuf,
+        /// Path to stylesheet within css dir
+        #[arg(short, long, value_name = "PATH")]
+        css: Option<PathBuf>,
         /// The path of the output html
         #[arg(short, long, value_name = "PATH")]
         export_path: Option<PathBuf>,
@@ -129,21 +69,7 @@ pub enum Action {
     },
 }
 
-// NOTE: If this struct stays like this (just containing two options) it might be better to just
-// have handle_actions return a bool
-
-/// Possible return values for [Cli::handle_actions]
-///
-/// Used to tell the application if it should continue or exit
-#[must_use = "ActionResult is used to tell the application what to do next."]
-pub enum ActionResult {
-    /// Exit the Application
-    Exit,
-    /// Continue as normal
-    Continue,
-}
-
-/// Custom errors that may be emited by [Cli::handle_actions]
+/// Custom errors that may occur when running different actions
 #[cfg(feature = "generate_config")]
 #[derive(Debug)]
 pub enum ActionError {
--- crates/igneous-md/src/main.rs
+++ crates/igneous-md/src/main.rs
@@ -14,12 +14,11 @@
 extern crate rocket;
 
 use clap::Parser;
-use rocket::{fs::FileServer, Build, Rocket};
+use rocket::fs::FileServer;
 use simple_logger::SimpleLogger;
 use std::{
     fs,
     path::PathBuf,
-    process::exit,
     sync::{Arc, Mutex},
 };
 
@@ -31,148 +30,184 @@ mod export;
 mod handlers;
 mod paths;
 
-use cli::{ActionResult, Cli};
+use cli::{Action, Cli};
 use handlers::*;
 use paths::default_css_dir;
 use paths::Paths;
 
 #[cfg(feature = "viewer")]
-use igneous_md_viewer::Viewer;
-#[cfg(feature = "viewer")]
-use std::thread;
+use {igneous_md_viewer::Viewer, std::thread};
 
 #[cfg(feature = "generate_config")]
-use std::{io, io::Write};
+use {
+    cli::ActionError,
+    std::{io, io::Write},
+};
 
-#[launch]
-fn rocket() -> Rocket<Build> {
+#[rocket::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
 
     SimpleLogger::new()
-        .with_level(cli.args.log_level.into())
+        .with_level(cli.log_level.into())
         .init()
         .expect("Failed to init Logger.");
 
-    match cli.handle_actions() {
-        Ok(ActionResult::Continue) => (),
-        Ok(ActionResult::Exit) => exit(0),
-        Err(e) => {
-            log::error!("{}", e);
-
-            exit(1)
-        }
-    }
-
-    // TODO: In the future it might be nice to check if the dir contains no css, rather than just
-    // checking if it exists. However as it stands currently users can avoid the prompt, by
-    // creating the dirs.
-
-    // Check if the config exists
-    if !default_css_dir().exists() {
-        // Always at least create the dir
-        if let Err(e) = fs::create_dir_all(default_css_dir().join("hljs")) {
-            log::error!(
-                "Failed to create css_dir: {} With error: {}",
-                default_css_dir().to_string_lossy(),
-                e
+    // Convert the md file rather than launching the server, if the user passed the subcommand
+    match cli.command {
+        Action::Convert {
+            path,
+            export_path,
+            css,
+        } => {
+            let html = convert::md_to_html(
+                &fs::read_to_string(path).expect("Failed to read md file to string."),
             );
 
-            exit(1)
-        }
+            if let Err(e) = export::export(
+                convert::initial_html(&css.clone().unwrap_or_default().to_string_lossy(), &html),
+                export_path.clone().map(PathBuf::from),
+            ) {
+                log::error!("Failed to export md.");
 
-        // If compiled with generate_config generate the config
+                return Err(Box::new(e) as Box<dyn std::error::Error>);
+            }
+
+            Ok(())
+        }
         #[cfg(feature = "generate_config")]
-        {
-            print!(
-                "No config found. Would you like to generate the default config? [(y)es/(N)o]: "
-            );
+        Action::GenerateConfig { overwrite } => {
+            if default_css_dir().exists() && !overwrite {
+                return Err(Box::new(ActionError::ConfigDirExists));
+            }
 
-            io::stdout().flush().expect("Failed to flush stdout.");
+            fs::create_dir_all(default_css_dir().join("hljs")).unwrap();
 
-            let mut user_input = String::new();
+            config::generate::generate_config_files(default_css_dir()).await?;
 
-            io::stdin()
-                .read_line(&mut user_input)
-                .expect("Failed to read input.");
+            Ok(())
+        }
+        Action::View {
+            path,
+            css,
+            css_dir,
+            port,
+            browser,
+            #[cfg(feature = "viewer")]
+            no_viewer,
+        } => {
+            // TODO: In the future it might be nice to check if the dir contains no css, rather than just
+            // checking if it exists. However as it stands currently users can avoid the prompt, by
+            // creating the dirs.
 
-            if user_input
-                .to_lowercase()
-                .chars()
-                .next()
-                .is_some_and(|c| c == 'y')
-            {
-                let rt = tokio::runtime::Runtime::new().unwrap();
+            // Check if the config exists
+            if !default_css_dir().exists() {
+                // Always at least create the dir
+                if let Err(e) = fs::create_dir_all(default_css_dir().join("hljs")) {
+                    log::error!(
+                        "Failed to create css_dir: {} With error: {}",
+                        default_css_dir().to_string_lossy(),
+                        e
+                    );
 
-                if let Err(e) =
-                    rt.block_on(config::generate::generate_config_files(default_css_dir()))
+                    return Err(Box::new(e));
+                }
+
+                // If compiled with generate_config generate the config
+                #[cfg(feature = "generate_config")]
                 {
-                    log::error!("Failed to create default config: {}", e);
+                    print!("No config found. Would you like to generate the default config? [(y)es/(N)o]: ");
 
-                    exit(1)
+                    io::stdout().flush().expect("Failed to flush stdout.");
+
+                    let mut user_input = String::new();
+
+                    io::stdin()
+                        .read_line(&mut user_input)
+                        .expect("Failed to read input.");
+
+                    if user_input
+                        .to_lowercase()
+                        .chars()
+                        .next()
+                        .is_some_and(|c| c == 'y')
+                    {
+                        if let Err(e) =
+                            config::generate::generate_config_files(default_css_dir()).await
+                        {
+                            log::error!("Failed to create default config: {}", e);
+
+                            return Err(e);
+                        }
+                    }
                 }
             }
+
+            let paths = match Paths::new(
+                path,
+                css_dir.unwrap_or(default_css_dir().to_path_buf()),
+                css.map(|p| PathBuf::from("/css").join(p)),
+            ) {
+                Ok(p) => p,
+                Err(e) => {
+                    log::error!("Failed to create Paths: {}", e);
+
+                    return Err(Box::new(e));
+                }
+            };
+
+            let config = match config::Config::new(&paths) {
+                Ok(mut c) => {
+                    c.start_watching()
+                        .expect("Failed to start watching config dir");
+                    Arc::new(Mutex::new(c))
+                }
+                Err(e) => {
+                    log::error!("Failed to create Config: {}", e);
+
+                    return Err(Box::new(e));
+                }
+            };
+
+            // The url of the md file, in the format:
+            // localhost:port
+            let address = format!("localhost:{}", port);
+
+            if browser && open::that_detached(&address).is_err() {
+                log::warn!("Failed to open browser");
+            }
+
+            #[cfg(feature = "viewer")]
+            if !no_viewer {
+                let client = Viewer::new(address);
+
+                thread::spawn(move || client.start());
+            }
+
+            rocket::build()
+                .configure(rocket::Config {
+                    port,
+                    log_level: cli.log_level.into(),
+                    ..rocket::Config::default()
+                })
+                .manage(paths)
+                .manage(config)
+                .mount("/", FileServer::from("."))
+                .mount(
+                    "/",
+                    routes![
+                        serve_main_js,
+                        serve_highlight_js,
+                        serve_css,
+                        get_initial_md,
+                        upgrade_connection,
+                    ],
+                )
+                .register("/", catchers![not_found, internal_error])
+                .launch()
+                .await?;
+
+            Ok(())
         }
     }
-
-    let paths = match Paths::new(
-        cli.args.path.expect("Path is required."),
-        cli.args.css_dir.unwrap_or(default_css_dir().to_path_buf()),
-        cli.args.css.map(|p| PathBuf::from("/css").join(p)),
-    ) {
-        Ok(p) => p,
-        Err(e) => {
-            log::error!("Failed to create Paths: {}", e);
-
-            exit(1)
-        }
-    };
-
-    let config = match config::Config::new(&paths) {
-        Ok(mut c) => {
-            c.start_watching()
-                .expect("Failed to start watching config dir");
-            Arc::new(Mutex::new(c))
-        }
-        Err(e) => {
-            log::error!("Failed to create Config: {}", e);
-
-            exit(1)
-        }
-    };
-
-    // The url of the md file, in the format:
-    // localhost:port
-    let address = format!("localhost:{}", cli.args.port);
-
-    if cli.args.browser && open::that_detached(&address).is_err() {
-        log::warn!("Failed to open browser");
-    }
-
-    #[cfg(feature = "viewer")]
-    if !cli.args.no_viewer {
-        let client = Viewer::new(address);
-
-        thread::spawn(move || client.start());
-    }
-
-    rocket::build()
-        .configure(rocket::Config {
-            port: cli.args.port,
-            log_level: cli.args.log_level.into(),
-            ..rocket::Config::default()
-        })
-        .manage(paths)
-        .manage(config)
-        .mount("/", FileServer::from("."))
-        .mount(
-            "/",
-            routes![
-                serve_main_js,
-                serve_highlight_js,
-                serve_css,
-                get_initial_md,
-                upgrade_connection,
-            ],
-        )
-        .register("/", catchers![not_found, internal_error])
 }
